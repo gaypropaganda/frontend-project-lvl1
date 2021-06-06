f771281cc9988b7c213652ccac5d242847cbb645
vimelon <vimelon131@gmail.com>
2021-06-06 17:58:20 +0700

    added eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import {Welcome} from '../src/cli.js';
-console.log("Welcome to the Brain Games!")
+import { Welcome } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 Welcome();
